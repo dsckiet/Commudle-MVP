83ad0abd9a9509b0ae15f70bef41df46c4fdac36
Aakanksha Shivani <shivaniaakanksha@gmail.com>
2019-10-20 01:12:07 +0530

    Intro Page customized

--- lib/routes/splash_screen.dart
+++ lib/routes/splash_screen.dart
@@ -1,6 +1,9 @@
+import 'package:commudle/routes/auth_screen.dart';
+import 'package:commudle/routes/intro_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -19,10 +22,25 @@ class _SplashScreenState extends State<SplashScreen> {
     Navigator.of(context).pushReplacementNamed('/IntroScreen');
   }
 
+  Future checkFirstSeen() async {
+    SharedPreferences prefsintro = await SharedPreferences.getInstance();
+    bool _seen = (prefsintro.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => AuthScreen()));
+    } else {
+      prefsintro.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => IntroScreen()));
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) => animation());
+    checkFirstSeen();
   }
 
   animation() {
@@ -34,40 +52,40 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(120),
-              child: Image(
-                image: AssetImage('assets/images/commudle-logo.png'),
-              ),
+        fit: StackFit.expand,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(120),
+            child: Image(
+              image: AssetImage('assets/images/commudle-logo.png'),
             ),
-            Container(
-              child: GestureDetector(
-                child: AnimatedOpacity(
-                  duration: Duration(milliseconds: 2300),
-                  opacity: this._opacity,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          const Color(0xFF4F73FF),
-                          const Color(0xFF28A6FF)
-                        ],
-                      ),
+          ),
+          Container(
+            child: GestureDetector(
+              child: AnimatedOpacity(
+                duration: Duration(milliseconds: 2300),
+                opacity: this._opacity,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        const Color(0xFF4F73FF),
+                        const Color(0xFF28A6FF)
+                      ],
                     ),
                   ),
                 ),
-                // onTap: () {
-                //   setState(() => this._opacity = 1.0 - this._opacity);
-                //   startTime();
-                // },
               ),
+              // onTap: () {
+              //   setState(() => this._opacity = 1.0 - this._opacity);
+              //   startTime();
+              // },
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
